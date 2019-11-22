7aab75a947c718e916301f7f198a9d5d4840717a
jenbuck <jrbuck31@gmail.com>
2019-11-21 20:16:20 -0600

    made 9 tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +9,57 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertTrue(test_Class.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void pairOfBracketsReturnsTrue() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertTrue(test_Class.hasBalancedBrackets("[Hello][World]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertFalse(test_Class.hasBalancedBrackets("{]"));
+    }
+
+    @Test
+    public void mismatchedBracketsReturnsFalse() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertFalse(test_Class.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void isNotNull() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertNotNull(test_Class.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void isNotTheSame() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertNotSame(test_Class.hasBalancedBrackets("[]"), test_Class.hasBalancedBrackets("{]"));
+    }
+
+    @Test
+    public void isTheSame() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertSame(test_Class.hasBalancedBrackets("[]}"), test_Class.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void isEqual() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertEquals(true, test_Class.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void isNotEqual() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertNotEquals(true, test_Class.hasBalancedBrackets("{]"));
+    }
 
 }
